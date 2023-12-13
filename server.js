f8b41285215676ff76bcc0a7f518a030e9a49413
abhioverhere <abhiabhilashpat@gmail.com>
2023-12-13 19:03:31 +0530

    deploy1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const adminRouter=require('./routes/admin.js');
 require('dotenv').config();
 
 const app= express();
-// app.use(express.static(path.join(__dirname,"frontend","build")));
+app.use(express.static(path.join(__dirname,'/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -22,10 +22,10 @@ mongoose.connect(mongoUrl)
   });
 
 //Setting basic backend API routes
-// app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"frontend","build","index.html"));});
 app.use('/user',userRouter);  
 app.use('/admin',adminRouter);  
 
+app.get("/*",(req,res)=>{res.sendFile(path.join(__dirname,'/build/index.html'));});
 const PORT = process.env.PORT || 4000;
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
