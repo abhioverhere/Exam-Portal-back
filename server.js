197d37e0d46a4dc479ec873dc895da6eaaa07105
abhioverhere <abhiabhilashpat@gmail.com>
2023-12-13 18:45:02 +0530

    files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const adminRouter=require('./routes/admin.js');
 require('dotenv').config();
 
 const app= express();
-app.use(express.static(path.join(__dirname,"frontend","build")));
+// app.use(express.static(path.join(__dirname,"frontend","build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -22,7 +22,7 @@ mongoose.connect(mongoUrl)
   });
 
 //Setting basic backend API routes
-app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"frontend","build","index.html"));});
+// app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"frontend","build","index.html"));});
 app.use('/user',userRouter);  
 app.use('/admin',adminRouter);  
 
